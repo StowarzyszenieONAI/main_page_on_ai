ae7b2cb99a11369bf5d4f490ec4860b58926d341
StowarzyszenieONAI <biuro@on-ai.pl>
2025-06-29 20:41:13 +0200

    Update send-contact-message.ts

--- api/send-contact-message.ts
+++ api/send-contact-message.ts
@@ -1,4 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import fetch from 'node-fetch';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
